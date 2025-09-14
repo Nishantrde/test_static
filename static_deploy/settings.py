4d6b34d27ea5b922e51e1cebd05f81e7ca7f1c53
Nishant <naishant2024rde@gmail.com>
2025-09-14 18:58:12 +0530

    Initial Commit

--- static_deploy/settings.py
+++ static_deploy/settings.py
@@ -132,7 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # settings.py (for production)
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
